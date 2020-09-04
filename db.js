ae1dbd56d8f26122c087f962dd0aa0a69a737600
Cintia Fumi <cintiafumi@gmail.com>
2020-09-04 20:25:31 -0300

    chore: db file

---
+++ db.js
@@ -0,0 +1,28 @@
+module.exports = {
+  tias: [
+    {
+      nome: 'Teresa',
+      idade: 71,
+      filhos: 3,
+      local: 'SP'
+    },
+    {
+      nome: 'Yolanda',
+      idade: 76,
+      filhos: 2,
+      local: 'MG'
+    },
+    {
+      nome: 'Margarida',
+      idade: 68,
+      filhos: 4,
+      local: 'SP'
+    },
+    {
+      nome: 'Neusa',
+      idade: 73,
+      filhos: 0,
+      local: 'SP'
+    },
+  ]
+}
